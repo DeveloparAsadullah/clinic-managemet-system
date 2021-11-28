6423d5ec610497e7ad67e60350bba8ef4a8ec508
Asadullah <95171035+DeveloparAsadullah@users.noreply.github.com>
2021-11-28 17:37:45 +0500

    Add files via upload

---
+++ adminController.cs
@@ -0,0 +1,398 @@
+﻿using clinic_management_system3.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace clinic_management_system3.Controllers
+{
+    public class adminController : Controller
+    {
+        clinic_management_system_2Entities db = new clinic_management_system_2Entities();
+        // GET: admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult admin()
+        {
+
+            return View();
+        }
+        [HttpPost]
+
+
+        //admin login code id password save in sql database
+        //id:muhammad.aasad110@gmail.com , pasword:asad123
+        public ActionResult admin(tbl_admin ad)
+        {
+
+            tbl_admin a = db.tbl_admin.Where(x => x.ad_email == ad.ad_email && x.ad_password == ad.ad_password).SingleOrDefault();
+            if (a!=null)
+            {
+                Session["name"] = a.ad_name;
+                Session["id"] = a.ad_id;
+                return RedirectToAction("category");
+            }
+            else
+            {
+                ViewBag.error = "invalid email and password";
+            }
+
+            return View();
+        }
+
+
+        //admin dashboard admin can view all categories and servises here(start)
+        public ActionResult category()
+        {
+
+            return View();
+        }
+
+        //admin dashboard admin can view all categories and servises here(end)
+
+
+
+
+        //admin add categories form this code (start)
+
+        public ActionResult add_category()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+
+       
+        public ActionResult add_category(tbl_cateogry ca, HttpPostedFileBase img)
+        {
+
+
+            if (Session["id"]!=null)
+            {
+                string path = upload_image(img);
+                if (path.Equals("-1"))
+                {
+
+                    ViewBag.error = "image not be uploaded";
+
+                }
+
+                else
+                {
+                    if (ModelState.IsValid==true)
+                    {
+                        tbl_cateogry c = new tbl_cateogry();
+
+                        c.cat_name = ca.cat_name;
+                        c.cat_status = 1;
+                        c.cat_image = path;
+                        c.ad_id_fk = Convert.ToInt32(Session["id"].ToString());
+                        db.tbl_cateogry.Add(c);
+                        db.SaveChanges();
+                        return RedirectToAction("category");
+                    }
+                    
+                }
+
+
+
+                
+            }
+
+            else
+            {
+                ViewBag.error2 = "without login you can not perform action";
+            }
+            return View();
+        }
+
+        //admin add categories form this code (end)
+
+        //admin can view category which he added (start)
+
+        public ActionResult view_department(int ? page)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.tbl_cateogry.Where(x => x.cat_status == 1).OrderByDescending(x => x.cat_id).ToList();
+            IPagedList<tbl_cateogry> cat = list.ToPagedList(pageindex, pagesize);
+
+
+            return View(cat);
+        }
+
+        //admin can view category which he added (end)
+
+
+        //admin can add products in selected category which he had added (start)
+
+        public ActionResult add_products()
+        {
+            List<tbl_cateogry> li = db.tbl_cateogry.ToList();
+            ViewBag.prod = new SelectList(li, "cat_id", "cat_name");
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult add_products(tbl_product pr,HttpPostedFileBase img )
+        {
+            List<tbl_cateogry> li = db.tbl_cateogry.ToList();
+            ViewBag.prod = new SelectList(li, "cat_id", "cat_name");
+
+            string path = upload_image(img);
+
+            if (Session["id"]!=null)
+            {
+                if (path.Equals("-1"))
+                {
+                    ViewBag.error = "imgage not uploaded";
+                }
+                else
+                {
+                    if (ModelState.IsValid==true)
+                    {
+                       
+
+                        tbl_product p = new tbl_product();
+                        tbl_admin a = new tbl_admin();
+
+                        p.pro_name = pr.pro_name;
+                        p.pro_price = pr.pro_price;
+                        p.pro_desc = pr.pro_desc;
+                        p.pro_image = path;
+                        p.ad_id_fk = Convert.ToInt32(Session["id"].ToString());
+                        p.cat_id_fk = pr.cat_id_fk;
+
+                        db.tbl_product.Add(p);
+                        db.SaveChanges();
+                        return RedirectToAction("category");
+                    }
+                    
+
+
+                }
+            }
+            else
+            {
+                ViewBag.error2 = "without login you can not perform action";
+            }
+
+           
+
+            return View();
+        }
+
+        //admin can add products in selected category which he had added (end)
+
+
+
+        //admin can view products in his dashboard  (start)
+        public ActionResult display_products(int? page,int ? id)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.tbl_product.Where(x => x.cat_id_fk == id).OrderByDescending(x => x.pro_id).ToList();
+            IPagedList<tbl_product> pro = list.ToPagedList(pageindex, pagesize);
+
+
+            return View(pro);
+        }
+
+
+        //admin can view products in his dashboard  (end)
+
+
+
+        //image uploading code(start)
+
+        public string upload_image(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+
+            int random = r.Next();
+
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                if (extension.ToLower().Equals(".png") || extension.ToLower().Equals(".jpg") || extension.ToLower().Equals(".jpeg"))
+                {
+
+                    try
+                    {
+                        path = Path.Combine(Server.MapPath("~/Content/upload"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+
+                        //throw;
+                    }
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('only jpeg ,jpg,png formate is allowed')</script>");
+
+                }
+
+
+            }
+            else
+            {
+                Response.Write("<script>alert('please select file')</script>");
+                path = "-1";
+
+            }
+
+            return path;
+        }
+
+        //image uploading code(end)
+
+
+
+
+        [HttpPost]
+       
+
+
+        //education application it is static
+
+        //public ActionResult edu_app()
+        //{
+
+        //    return View();
+        //}
+
+
+        public ActionResult education_application()
+        {
+
+            return View();
+        }
+        //end
+
+        //admin can view user details (start)
+
+        public ActionResult user_details()
+        {
+
+            List<tbl_user> li = db.tbl_user.ToList();
+            List<tbl_user> us = li.Select(x => new tbl_user
+            {
+                u_name=x.u_name,
+                u_contact=x.u_contact,
+                u_email=x.u_email,
+                u_password=x.u_password,
+                
+            }
+            
+            ).ToList();
+             return View(us);
+        }
+
+        //admin can view user details (end)
+
+
+        //admin can view user feedback (start)
+        public ActionResult view_feedback()
+        {
+            
+
+            List<tbl_feedback> lid = db.tbl_feedback.ToList();
+            List<feedbackdetails> lvm = lid.Select(x => new feedbackdetails
+
+            {
+                name=x.name,
+                email=x.email,
+                contact_number=x.contact_number,
+                feedback=x.feedback,
+               
+                sugestion =x.sugestion,
+                
+
+            }).ToList();
+
+
+            return View(lvm);
+        }
+
+        //admin can view user feedback (end)
+
+        //admin can view user contact  (start)
+
+        public ActionResult user_contact_details()
+        {
+            List<tbl_contact> cl = db.tbl_contact.ToList();
+            List<tbl_contact> con = cl.Select(x => new tbl_contact
+            {
+               first_name=x.first_name,
+               last_name=x.last_name,
+               email_id=x.email_id,
+               sms=x.sms,
+               
+
+
+
+            }).ToList();
+
+
+
+            return View(con);
+        }
+
+        //admin can view user contact  (end)
+
+
+        //admin can view how much products are purchased by users  (start)
+        public ActionResult bussiness()
+        {
+
+            List<tbl_order> o = db.tbl_order.ToList();
+
+            List<bussiness_model> lvm = o.Select(x => new bussiness_model
+
+            {
+                buyers_name=x.buyers_name,
+                ord_productname = x.ord_productname,
+                ord_amount = x.ord_amount,
+                ord_quantity = x.ord_quantity,
+                order_date = x.order_date,
+                ord_id = x.ord_id,
+                
+
+
+
+            }).ToList();
+
+
+
+            return View(lvm);
+        }
+
+
+        //admin can view how much products are purchased by user?  (end)
+
+
+
+    }
+}
---
+++ userController.cs
@@ -0,0 +1,388 @@
+﻿using clinic_management_system3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace clinic_management_system3.Controllers
+{
+    public class userController : Controller
+    {
+        clinic_management_system_2Entities db = new clinic_management_system_2Entities();
+        // GET: user
+
+
+        //user dashboard code all categories can be seen here by user(start)
+        public ActionResult Index(int? page)
+        {
+
+
+            if (TempData["cart"] != null)
+            {
+
+                double x = 0;
+
+                List<Cadd_to_cart_class> li2 = TempData["cart"] as List<Cadd_to_cart_class>;
+
+                foreach (var item in li2)
+                {
+
+                    x += Convert.ToInt32(item.total_bill);
+
+
+                }
+
+                TempData["total"] = x;
+
+
+            }
+            TempData.Keep();
+
+
+
+
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.tbl_cateogry.Where(x => x.cat_status == 1).OrderByDescending(x => x.cat_id).ToList();
+            IPagedList<tbl_cateogry> cat = list.ToPagedList(pageindex, pagesize);
+
+
+            return View(cat);
+        }
+
+        //user dashboard code all categories can be seen here by user(end)
+
+
+        //about us page for user it is static
+        public ActionResult aboutus()
+        {
+
+            return View();
+        }
+
+
+        //user can register here code(start)
+        public ActionResult signup()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult signup(tbl_user us)
+        {
+            if (ModelState.IsValid == true)
+            {
+                tbl_user u = new tbl_user();
+                u.u_name = us.u_name;
+                u.u_email = us.u_email;
+                u.u_password = us.u_password;
+                u.u_contact = us.u_contact;
+                //u.ad_id_fk = us.ad_id_fk;
+                db.tbl_user.Add(u);
+                db.SaveChanges();
+                return RedirectToAction("login");
+            }
+
+
+            return View();
+        }
+
+        //user can register here code(end)
+
+
+        //user login code(start)
+        public ActionResult login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult login(tbl_user us)
+        {
+
+            tbl_user u = db.tbl_user.Where(x => x.u_password == us.u_password && x.u_email == us.u_email).SingleOrDefault();
+            if (u != null)
+            {
+               
+                Session["u_name"] = u.u_name;
+                Session["u_id"] = u.u_id;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.error = "invalid email id and password";
+            }
+
+
+            return View();
+        }
+
+        //user login code(end)
+
+
+
+
+        //user giving feedback code(start)
+        public ActionResult feedback()
+        {
+            List<tbl_gendar> li = db.tbl_gendar.ToList();
+            ViewBag.gen = new SelectList(li, "gen_id", "gendar");
+
+            List<tbl_country> lic = db.tbl_country.ToList();
+            ViewBag.cnt = new SelectList(lic, "cnt_id", "cnt_name");
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult feedback(feedback_model fm)
+        {
+            if (ModelState.IsValid == true)
+            {
+                List<tbl_gendar> li = db.tbl_gendar.ToList();
+                ViewBag.gen = new SelectList(li, "gen_id", "gendar");
+
+                List<tbl_country> lic = db.tbl_country.ToList();
+                ViewBag.cnt = new SelectList(lic, "cnt_id", "cnt_name");
+
+
+                tbl_feedback f = new tbl_feedback();
+                f.name = fm.name;
+                f.u_id_fk = Convert.ToInt32(Session["u_id"]);
+                f.email = fm.email;
+                f.contact_number = fm.contact_number;
+                f.feedback = fm.feedback;
+                f.sugestion = fm.sugestion;
+                f.gen_id_fk = fm.gen_id_fk;
+                f.cnt_id_fk = fm.cnt_id_fk;
+
+                db.tbl_feedback.Add(f);
+                db.SaveChanges();
+                return RedirectToAction("feedback");
+
+            }
+
+            return View();
+        }
+
+        //user giving feedback code(end)
+
+
+        //user contact us code(start)
+        public ActionResult contactus()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult contactus(tbl_contact cn)
+        {
+            if (ModelState.IsValid == true)
+            {
+                tbl_contact c = new tbl_contact();
+                c.first_name = cn.first_name;
+                c.last_name = cn.last_name;
+                c.email_id = cn.email_id;
+                c.sms = cn.sms;
+                db.tbl_contact.Add(c);
+                db.SaveChanges();
+                return RedirectToAction("contactus");
+            }
+
+
+            return View();
+        }
+
+        //user contact us code(end)
+
+
+
+
+
+
+        //user can view categories here code(start)
+        public ActionResult display_pro_foruser(int? page, int? id)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.tbl_product.Where(x => x.cat_id_fk == id).OrderByDescending(x => x.pro_id).ToList();
+            IPagedList<tbl_product> pro = list.ToPagedList(pageindex, pagesize);
+
+
+            return View(pro);
+        }
+
+
+        //search products code (start)
+        [HttpPost]
+        public ActionResult display_pro_foruser(int ? page,int ? id , string search )
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.tbl_product.Where(x => x.pro_name.Contains(search)).OrderByDescending(x => x.pro_id).ToList();
+            IPagedList<tbl_product> pros = list.ToPagedList(pageindex, pagesize); 
+
+
+
+            return View(pros);
+        }
+
+        //search products code (end)
+
+
+
+        //user can view categories here code(end)
+
+        //user can view details of products in selective categories here code(start)
+
+        public ActionResult details_of_pro(int? id)
+        {
+            tbl_product p = db.tbl_product.Where(x => x.pro_id == id).SingleOrDefault();
+            product_details_class pr = new product_details_class();
+            pr.pro_id = p.pro_id;
+            pr.pro_name = p.pro_name;
+            pr.pro_price = p.pro_price;
+            pr.pro_image = p.pro_image;
+            pr.pro_desc = p.pro_desc;
+
+            tbl_cateogry c = db.tbl_cateogry.Where(x => x.cat_id == p.cat_id_fk).SingleOrDefault();
+            pr.cat_name = c.cat_name;
+
+
+
+            return View(pr);
+        }
+        //user can view details of products in selective categories here code(end)
+
+
+
+        //add to cart code start
+        public ActionResult add_to_cart(int? id)
+        {
+
+            tbl_product p = db.tbl_product.Where(x => x.pro_id == id).SingleOrDefault();
+
+
+            return View(p);
+        }
+
+        List<Cadd_to_cart_class> li = new List<Cadd_to_cart_class>();
+        [HttpPost]
+
+
+        public ActionResult add_to_cart(int? id, string qty)
+        {
+            List<Cadd_to_cart_class> li = new List<Cadd_to_cart_class>();
+
+            tbl_product p = db.tbl_product.Where(x => x.pro_id == id).SingleOrDefault();
+            Cadd_to_cart_class ac = new Cadd_to_cart_class();
+            ac.pro_id = p.pro_id;
+            ac.ord_productname = p.pro_name;
+            ac.pro_price = p.pro_price;
+            ac.pro_desc = p.pro_desc;
+            ac.ord_quantity = Convert.ToInt32(qty);
+            ac.total_bill = ac.pro_price * ac.ord_quantity;
+
+            if (TempData["cart"] == null)
+            {
+                li.Add(ac);
+                TempData["cart"] = li;
+
+            }
+            else
+            {
+                List<Cadd_to_cart_class> li2 = TempData["cart"] as List<Cadd_to_cart_class>;
+                li2.Add(ac);
+            }
+            TempData.Keep();
+            return RedirectToAction("Index");
+
+
+
+            return View();
+        }
+
+
+        //add to cart code (end)
+
+
+        //checkout and shop[ now code (start)
+        public ActionResult checkout()
+        {
+            List<Cadd_to_cart_class> li2 = TempData["cart"] as List<Cadd_to_cart_class>;
+            TempData.Keep();
+
+           
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult checkout(tbl_order o)
+        {
+            List<Cadd_to_cart_class> li2 = TempData["cart"] as List<Cadd_to_cart_class>;
+
+
+
+            tbl_invoice iv = new tbl_invoice();
+            iv.u_id_fk = Convert.ToInt32(Session["u_id"].ToString());
+            iv.inv_date = System.DateTime.Now;
+            iv.total_bill = (double)TempData["total"];
+            db.tbl_invoice.Add(iv);
+            db.SaveChanges();
+
+            foreach (var item in li2)
+            {
+                tbl_order od = new tbl_order();
+                od.buyers_name = Session["u_name"].ToString();
+                od.pr_id_fk = item.pro_id;
+                od.ord_productname = item.ord_productname;
+                od.order_date = System.DateTime.Now;
+                od.ord_quantity = item.ord_quantity;
+                od.ord_amount = item.pro_price;
+                db.tbl_order.Add(od);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+
+            TempData.Keep();
+            return View("");
+
+        }
+
+        //checkout and shop[ now code (end)
+
+
+        //logout code (start)
+        public ActionResult logout()
+        {
+            Session.Abandon();
+            Session.RemoveAll();
+            return RedirectToAction("login");
+        }
+
+
+        //end
+        
+
+
+
+    }
+
+
+}
